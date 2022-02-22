346ef8ba516f0d5f02b1d393dffe084ccd7b4d51
aogu <aogu89goo415@gmail.com>
2022-02-22 17:09:56 +1100

    SQL tutorial

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import ToDoList, Item
 # Create your views here.
 
-def index(response):
-    return HttpResponse("<h1>tech with tim!</h1>")
+def index(response, name):
+    ls = ToDoList.objects.get(name=name)
+    item = ls.item_set.get(id=1)
+    return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" % (ls.name, str(item.text)))
 
-def v1(response):
-    return HttpResponse("<h1>view 1! </h1>")
+
 
 
