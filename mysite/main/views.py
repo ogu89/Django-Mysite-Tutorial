42f400b1efedd2c02dfd7d50de4bdfa84c70b814
aogu <aogu89goo415@gmail.com>
2022-02-22 18:21:40 +1100

    admin dashboard

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -7,7 +7,3 @@ def index(response, name):
     ls = ToDoList.objects.get(name=name)
     item = ls.item_set.get(id=1)
     return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" % (ls.name, str(item.text)))
-
-
-
-
