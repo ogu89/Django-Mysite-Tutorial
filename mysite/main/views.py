5168434f92cc6b02a212f671fd809e0ccf431bd4
aogu <aogu89goo415@gmail.com>
2022-02-25 19:47:59 +1100

    Custom forms

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -7,6 +7,29 @@ from .forms import CreateNewList
 def index(response, id):
     
     ls = ToDoList.objects.get(id=id)
+    
+    # {"save": ["save"], "c1": ["clicked"]}
+    
+    if response.method == "POST" : 
+        print(response.POST)
+        if response.POST.get("save"):
+            for item in ls.item_set.all():
+                if response.POST.get("c" + str(item.id)) == "clicked":
+                    item.complete = True
+                else:
+                    item.complete = False
+                     
+                item.save()
+        
+        elif response.POST.get("newItem"):
+            txt = response.POST.get("new")
+            
+            if len(txt) > 2:
+                ls.item_set.create(text=txt, complete = False)
+            else:
+                print("invalid")
+            
+    
     return render(response, "main/list.html", {"ls":ls})
 
 def home(response):
@@ -26,5 +49,3 @@ def create(response):
         form = CreateNewList()
     return render(response, "main/create.html", {"form": form})
 
- 
- 
