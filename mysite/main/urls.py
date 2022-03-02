def0bf1bf659d7cefc5efa33c215505943907766
aogu <aogu89goo415@gmail.com>
2022-03-02 19:51:55 +1100

    having one error at the end of this video

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -1,4 +1,3 @@
-from unicodedata import name
 from django.urls import path
 
 from . import views
@@ -7,5 +6,6 @@ urlpatterns = [
     path("<int:id>", views.index, name="index"),
     path("", views.home, name="home"),
     path("create/", views.create, name="create"),
+    path("view/", views.view, name="view"),
 ] 
 
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -5,37 +5,40 @@ from .forms import CreateNewList
 # Create your views here.
 
 def index(response, id):
-    
     ls = ToDoList.objects.get(id=id)
     
-    # {"save": ["save"], "c1": ["clicked"]}
-    
-    if response.method == "POST" : 
-        print(response.POST)
-        if response.POST.get("save"):
-            for item in ls.item_set.all():
-                if response.POST.get("c" + str(item.id)) == "clicked":
-                    item.complete = True
-                else:
-                    item.complete = False
-                     
-                item.save()
+    if ls in response.user.todolist.all():
         
-        elif response.POST.get("newItem"):
-            txt = response.POST.get("new")
-            
-            if len(txt) > 2:
-                ls.item_set.create(text=txt, complete = False)
-            else:
-                print("invalid")
-            
     
-    return render(response, "main/list.html", {"ls":ls})
+        # {"save": ["save"], "c1": ["clicked"]}
+        
+        if response.method == "POST" : 
+            print(response.POST)
+            if response.POST.get("save"):
+                for item in ls.item_set.all():
+                    if response.POST.get("c" + str(item.id)) == "clicked":
+                        item.complete = True
+                    else:
+                        item.complete = False
+                        
+                    item.save()
+            
+            elif response.POST.get("newItem"):
+                txt = response.POST.get("new")
+                
+                if len(txt) > 2:
+                    ls.item_set.create(text=txt, complete = False)
+                else:
+                    print("invalid")
+            
+        return render(response, "main/list.html", {"ls":ls})
+    return render(response, "main/view.html", {"ls":ls})
 
 def home(response):
     return render(response, "main/home.html", {})
 
 def create(response):
+    response.user
     if response.method == "POST": 
         form = CreateNewList(response.POST)
         
@@ -43,9 +46,12 @@ def create(response):
             n = form.cleaned_data["name"]
             t = ToDoList(name=n)
             t.save()
+            response.user.todolist.add(t)
             
         return HttpResponseRedirect("/%i" %t.id)
     else:
         form = CreateNewList()
     return render(response, "main/create.html", {"form": form})
 
+def view(response):
+    return render(response, "main/view.html", {})
